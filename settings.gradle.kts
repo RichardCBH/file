9610efc5d69e0a3fdca69a1d255ccf0021aa2a95
RichardCBH <caibinhong@126.com>
2026-07-03 11:43:42 +0800

    Explicitly register version catalog 'libs' from gradle/libs.versions.toml so alias() works in build files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,11 +5,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
     }
 }
 
